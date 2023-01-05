d753a71a5b045c80ecf7e297a03c153687988563
widyacantikk <107343788+Ohwdyaa@users.noreply.github.com>
2023-01-05 10:09:09 +0800

    Add files via upload

---
+++ deep_learning.py
@@ -0,0 +1,347 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+import shutil
+from tqdm.notebook import tqdm as tq
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import layers, Sequential
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Conv2D, Activation, MaxPooling2D, Dropout, Flatten, Dense
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras import layers,Sequential
+from tensorflow.keras.models import Model
+
+
+
+# # memasukkan folder yang nantinya di jadikan ke dataset
+mypath = 'Mazda_car/'
+
+file_name = []
+tag = []
+full_path = []
+for path, subdirs, files in os.walk(mypath):
+    for name in files:
+        full_path.append(os.path.join(path, name).replace("\\","/"))
+        tag.append(path.split('/')[-1])
+        file_name.append(name)
+
+df = pd.DataFrame({'path': full_path, 'file_name': file_name, 'tag':'tag'})
+df.groupby(['tag']).size()
+df
+df.head()
+df.tail()
+
+
+# # train data
+X = df['path']
+y = df['tag']
+
+
+# memisahkan split data antara train, test dan validation
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+# kemudian:
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.3, random_state=1)
+
+
+df_tr = pd.DataFrame({'path': X_train, 'tag': y_train, 'set':'train'})
+df_te = pd.DataFrame({'path':X_test, 'tag':y_test, 'set':'test'})
+df_val = pd.DataFrame({'path': X_val, 'tag': y_val, 'set': 'validation'})
+print('train_size', len(df_tr))
+print('test_size', len(df_te))
+print('val size', len(df))
+
+df_all = df_tr.append([df_te, df_val]).reset_index(drop=1)
+print('=====================================================\n')
+print(df_all.groupby(['set', 'tag']).size(), '\n')
+print('========================================================\n')
+df_all.sample(3)
+
+
+## merapikan ke folder masing masing 
+datasource_path = "Toyota_car/"
+dataset_path = "dataset/"
+
+for index, row in tq(df_all.iterrows(), ):
+    
+    #detect filepath
+    file_path = row['path']
+    if os.path.exists(file_path) == False:
+            file_path = os.path.join(datasource_path,row['tag'],row['image'].split('.')[0])            
+    
+    #make folder destination dirs
+    if os.path.exists(os.path.join(dataset_path,row['set'],row['tag'])) == False:
+        os.makedirs(os.path.join(dataset_path,row['set'],row['tag']))
+    
+    #define file dest
+    destination_file_name = file_path.split('/')[-1]
+    file_dest = os.path.join(dataset_path,row['set'],row['tag'],destination_file_name)
+    
+    #copy file from source to dest
+    if os.path.exists(file_dest) == False:
+        shutil.copy2(file_path,file_dest)
+
+
+
+# Define Input Parameters
+dim = (160, 160)
+# dim = (456, 456)
+channel = (3, )
+input_shape = dim + channel
+
+#batch size
+batch_size = 16
+
+#Epoch
+epoch = 10
+
+
+# membuat dan mendifinisikan dataset generator
+train_datagen = ImageDataGenerator(rescale=1. / 255,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True)
+
+val_datagen = ImageDataGenerator(rescale=1. / 255,
+                                 shear_range=0.2,
+                                 zoom_range=0.2,
+                                 horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1. / 255,
+                                  shear_range=0.2,
+                                  zoom_range=0.2,
+                                  horizontal_flip=True)
+
+
+#mendifinisikan asal folder berasal
+train_generator = train_datagen.flow_from_directory('dataset/train/',
+                                                    target_size=dim,
+                                                    batch_size=batch_size,
+                                                    class_mode='categorical',
+                                                    shuffle=True)
+
+val_generator = val_datagen.flow_from_directory('dataset/validation/',
+                                                target_size=dim,
+                                                batch_size=batch_size,
+                                                class_mode='categorical',
+                                                shuffle=True)
+
+test_generator = test_datagen.flow_from_directory('dataset/test/',
+                                                  target_size=dim,
+                                                  batch_size=batch_size,
+                                                  class_mode='categorical',
+                                                  shuffle=True)
+
+num_class = test_generator.num_classes
+labels = train_generator.class_indices.keys()
+print(labels)
+
+
+# membuat tf.data untuk kompabilitas yang lebih baik
+def tf_data_generator(generator, input_shape):
+    num_class = generator.num_classes
+    tf_generator = tf.data.Dataset.from_generator(
+        lambda: generator,
+        output_types=(tf.float32, tf.float32),
+        output_shapes=([None
+                        , input_shape[0]
+                        , input_shape[1]
+                        , input_shape[2]]
+                       ,[None, num_class])
+    )
+    return tf_generator
+train_data = tf_data_generator(train_generator, input_shape)
+test_data = tf_data_generator(test_generator, input_shape)
+val_data = tf_data_generator(val_generator, input_shape)
+
+
+#membuat struktur CNN
+model = Sequential()
+model.add(Conv2D(128, (3, 3), padding='same', input_shape=input_shape))
+
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_class))
+model.add(Activation('softmax'))
+
+# Compile the model
+print('Compiling Model.......')
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+model.summary()
+
+
+#visualize the final model
+model_viz = tf.keras.utils.plot_model(model,
+                          to_file='model.png',
+                          show_shapes=True,
+                          show_layer_names=True,
+                          rankdir='TB',
+                          expand_nested=True,
+                          dpi=55)
+model_viz
+# Train Model
+EPOCH = 2
+
+# create training and validation loss visualisasi
+history = model.fit(x=train_data,
+        steps_per_epoch=len(train_generator),
+        epochs=EPOCH,
+        validation_data=val_data,
+        validation_steps=len(val_generator), 
+        shuffle=True,
+        verbose = 1)
+
+
+# Plot history: MAE
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.title('Training and Validation Loss')
+plt.ylabel('value')
+plt.xlabel('No. epoch')
+plt.legend(loc="upper left")
+plt.show()
+
+# Plot history: MSE
+plt.plot(history.history['accuracy'], label='Training Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+plt.title('Training and Validation Accuracy')
+plt.ylabel('value')
+plt.xlabel('No. epoch')
+plt.legend(loc="upper left")
+plt.show()
+
+
+# Evaluate Models
+loss, acc = model.evaluate(train_data, steps=len(train_generator), verbose=0)
+print('Accuracy on training data: {:.4f} \nLoss on training data: {:.4f}'.format(acc,loss),'\n')
+ 
+loss, acc = model.evaluate(test_data, steps=len(test_generator), verbose=0)
+print('Accuracy on test data: {:.4f} \nLoss on test data: {:.4f}'.format(acc,loss),'\n')   
+
+
+#____IMPLEMENTASI VGG16____
+import keras
+import keras_applications
+import tensorflow as tf
+from keras.applications.vgg16 import VGG16
+import matplotlib.image as mpimg
+
+
+#Definisikan dataset
+base_dir = 'deep_learning_21\mazda_car'
+print(os.listdir(base_dir))
+
+
+# Menghitung jumlah gambar pada dataset
+number_label = {}
+total_files = 0
+for i in os.listdir(base_dir):
+    counting = len(os.listdir(os.path.join(base_dir, i)))
+    number_label[i] = counting
+    total_files += counting
+print("Total Files : " + str(total_files))
+
+
+# Menampilkan sampel gambar tiap kelas
+img_each_class = 1
+img_samples = {}
+classes = list(number_label.keys())
+
+for c in classes:
+    temp = os.listdir(os.path.join(base_dir, c))[:img_each_class]
+    for item in temp:
+        img_path = os.path.join(base_dir, c, item)
+        img_samples[c] = img_path
+
+for i in img_samples:
+    fig = plt.gcf()
+    img = mpimg.imread(img_samples[i])
+    plt.title(i)
+    plt.imshow(img)
+    plt.show()
+
+IMAGE_SIZE = (200, 200)
+BATCH_SIZE = 32
+SEED = 999
+# Image Augmentation
+data_augmentation = tf.keras.Sequential(
+    [
+        tf.keras.layers.RandomFlip("horizontal",
+                                   input_shape=(IMAGE_SIZE[0],
+                                                IMAGE_SIZE[1],
+                                                3)),
+        tf.keras.layers.RandomRotation(0.1),
+        tf.keras.layers.RandomZoom(0.1),
+        tf.keras.layers.Rescaling(1. / 255)
+    ]
+)
+
+## Loading VGG16 model
+base_vgg_model = VGG16(weights="imagenet", include_top=False, input_shape=(IMAGE_SIZE[0], IMAGE_SIZE[1], 3))
+base_vgg_model.trainable = False
+
+# Preprocessing Input
+vgg_preprocess = tf.keras.applications.vgg16.preprocess_input
+train_data.preprocessing_function = vgg_preprocess
+
+# Transfer learning dengan VGG16
+vgg_model = tf.keras.models.Sequential([
+    data_augmentation,
+    base_vgg_model,
+    tf.keras.layers.Dropout(0.7),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(5, activation='softmax')
+])
+
+# Compiling model
+vgg_model.compile(
+    loss='categorical_crossentropy',
+    optimizer=tf.keras.optimizers.Adam(),
+    metrics=['accuracy']
+)
+"""#### Evaluasi Model"""
+
+# Membuat plot akurasi model VGG16
+plt.figure(figsize=(10, 4))
+# plt.plot(vgg_hist.history['accuracy'])
+# plt.plot(vgg_hist.history['val_accuracy'])
+# plt.title('VGG16 model accuracy')
+# plt.ylabel('accuracy')
+# plt.xlabel('epoch')
+# plt.legend(['train', 'test'], loc='upper left')
+# plt.grid(True)
+# plt.show()
+
+print()
+
+# Membuat plot loss model VGG16
+plt.figure(figsize=(10, 4))
+# plt.plot(vgg_hist.history['loss'])
+# plt.plot(vgg_hist.history['val_loss'])
+# plt.title('VGG16 model loss')
+# plt.ylabel('loss')
+# plt.xlabel('epoch')
+# plt.legend(['train', 'test'], loc='upper left')
+# plt.grid(True)
+# plt.show()
